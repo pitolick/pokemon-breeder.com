1b7379147004337b43bfc6e2ca4d0673a1c49148
pitolick <pi-to.rick@hotmail.co.jp>
2016-05-22 15:19:23 +0900

    Revert "Git導入"
    This reverts commit 798212cb4ae266f44de2b6f1c478c25e752433b1.

--- goodtemp/js/tel.js
+++
@@ -1,8 +0,0 @@
-jQuery(function($) {
-	$(function(){
-		var device = navigator.userAgent;
-		if((device.indexOf('iPhone') > 0 && device.indexOf('iPad') == -1) || device.indexOf('iPod') > 0 || device.indexOf('Android') > 0){
-			$(".tel").wrap('<a href="tel:0667066822"></a>');
-		}
-	});
-});
